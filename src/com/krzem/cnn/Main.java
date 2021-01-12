cca94ff24b96ced060d585c438605ae076f17fa8
Krzem <krzem5.dev@gmail.com>
2021-01-12 19:59:27 +0100

    Push Update 01/12/2021, 19:51:23

--- src/com/krzem/cnn/Main.java
+++ src/com/krzem/cnn/Main.java
@@ -1,10 +1,10 @@
-import com.krzem.cnn.*;
+package com.krzem.cnn;
 import java.util.ArrayList;
 import java.util.List;
 
 
 
-public class test{
+public class Main{
 	public static void main(String[] args){
 		List<Layer> ll=new ArrayList<Layer>();
 		ll.add(new ConvolutionLayer(20,3,5,5,"sigmoid"));
@@ -22,57 +22,3 @@ public class test{
 		System.out.println(cnn.classify(Dataset.get_img("./images/train/watch_0001.jpg")));
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import com.krzem.cnn.*;
-// import java.util.ArrayList;
-// import java.util.List;
-
-
-
-// public class test{
-// 	public static void main(String[] args){
-// 		List<Layer> ll=new ArrayList<Layer>();
-// 		ll.add(new ConvolutionLayer(5,5,4,20));
-// 		ll.add(new PoolingLayer(2,2));
-// 		ll.add(new ConvolutionLayer(5,5,1,20));
-// 		ll.add(new PoolingLayer(2,2));
-// 		ll.add(new ConvolutionLayer(3,3,1,20));
-// 		ArrayList<String> cL=new ArrayList<String>();
-// 		cL.add("airplanes");
-// 		cL.add("butterfly");
-// 		cL.add("flower");
-// 		cL.add("grand_piano");
-// 		cL.add("watch");
-// 		CNN cnn=new CNN(32,32,ll,300,1,cL,"relu",0.01);
-// 		System.out.println(cnn.classify(Dataset.get_img("./images/trainset/airplanes_0001.jpg")));
-// 		cnn.train(Dataset.load("./images/trainset",4),Dataset.load("./images/tuneset",4),3,5,true);
-// 		System.out.println(cnn.classify(Dataset.get_img("./images/trainset/airplanes_0001.jpg")));
-// 	}
-// }
