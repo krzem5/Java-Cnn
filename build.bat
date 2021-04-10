c6776c7611bec15350fffa7d23f2a53bd8297d22
Krzem <krzem5.dev@gmail.com>
2021-04-10 09:18:24 +0200

    Push Update 04/10/2021, 09:17:57

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/cnn/Main.java&&jar cvmf ../manifest.mf ../build/cnn.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="cnn.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/cnn.jar
-:end
+python build.py %*
